50f3db2731a4a0e0d5906767bb087669be143751
Hostin1 <hostinwilmer2015@hotmail.com>
2019-05-10 10:08:19 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title>Ejemplo</title>
+</head>
+<body>
+
+<?php 
+
+ $nombre = "Hostin";
+ $edad = 28;
+ $altura = 180;
+ $MayorEdad = True;
+
+ echo "mi nombre es ".$nombre. " tengo ".$edad."años"." mido ".$altura." cm";
+
+ ?>
+ 
+
+</body>
+</html>
